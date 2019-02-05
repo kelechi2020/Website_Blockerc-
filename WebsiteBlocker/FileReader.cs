315c1c53cfd59f5eed54b91391baa0ce6cb51f3c
KELECHI <egbosikelechi@gmail.com>
2019-02-05 13:31:52 +0100

    First Commit

---
+++ WebsiteBlocker/FileReader.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace WebsiteBlocker
+{
+    public static class FileReader
+    {
+        public static string redirect = "127.0.0.1";
+        public static string hostPath = @"E:\hosts";
+
+        public static string[] websites = { "account.jetbrains.com", "www.google.com", "google.com", "facebook.com", "nairaland.com", "www.nairaland.com", "http://www.su-kam.com/load-chart.aspx" };
+
+        public static void BlockSites()
+        {
+
+            FileStream fs = new FileStream(hostPath, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.Read);
+            StreamReader hostFile = new StreamReader(fs);
+            StreamWriter sw = new StreamWriter(fs);
+
+            string file = hostFile.ReadToEnd();
+
+
+            foreach (var website in websites)
+            {
+
+                if (!file.Contains(website))
+                {
+                    sw.WriteLine($"{redirect}  {website}");
+                    sw.Flush();
+
+                }
+            }
+
+        }
+
+        public static void UnblockSites()
+        {
+            string[] file = File.ReadAllLines(hostPath);
+            foreach (var website in websites)
+            {
+                file = file.Where(x => x != website).ToArray();                
+            }
+
+            foreach (var item in file)
+            {
+                Console.WriteLine(item);
+            }
+
+            File.WriteAllLines(hostPath, file);
+        }
+    }
+}
--- WebsiteBlocker/Program.cs
+++ WebsiteBlocker/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,22 @@ namespace WebsiteBlocker
     {
         static void Main(string[] args)
         {
+      
+            DateTime result = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day);
+
+
+            if ((result + new TimeSpan(9, 0, 0)).Hour < DateTime.Now.Hour && DateTime.Now.Hour < (result + new TimeSpan(11, 0, 0)).Hour)
+            {
+                FileReader.BlockSites();
+            }
+            else
+            {
+                FileReader.UnblockSites();
+            }
+
+
+
+
         }
     }
 }
